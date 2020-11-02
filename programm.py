d64c49e72eb1558b6df56e811a267dd0d6018cfe
celiltan7 <73793746+celiltan7@users.noreply.github.com>
2020-11-02 19:18:17 +0100

    Update programm.py
    Anfang vom Programm

--- programm.py
+++ programm.py
@@ -1,28 +1,9 @@
-from tkinter import *
-from tkinter.ttk import *
-
-w=Tk()
-w.title("Yokai-Serverüberwachung")
-w.attributes("-fullscreen", True)
-
-style = Style()
-
-style.configure ("W.TButton", font = 
-                ("Bebas", 20, "bold", "underline"),
-                fg = "ba2323")
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
 
 
-buttonE = Button(w,
-                text='Exit!',
-                style = "W.TButton",
-                font=('Bebas', '30'),
-                command=sys.exit,
-                fg="#ba2323",
-                bg="#201f1f",
-                )
-buttonE.grid(row = 0, column =3, padx = 100)
-buttonE.place(x=800, y=800)
-buttonE.config( width=250, height=250)
-buttonE.pack()
-
-w.mainloop()
+def Fenster():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry()
