3e3b4f4fb7e50c39bcc0d50d07584d8e2df67e67
celiltan7 <73793746+celiltan7@users.noreply.github.com>
2020-12-15 00:51:06 +0100

    TemperaturButton

--- yokai.py
+++ yokai.py
@@ -25,22 +25,27 @@ for row in myresult:
 #Tabelle
 
 
+##############################################################
+#Bildschirm
+
 
 class UIWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setGeometry(600, 350, 800, 480)
         MainWindow.setFixedSize(800, 480)
         MainWindow.setStyleSheet("background-color: #201f1f;")
-        MainWindow.setWindowIcon(QtGui.QIcon('hintergrund.png'))
+        MainWindow.setWindowIcon(QtGui.QIcon('favicon.png'))
         self.label = QtWidgets.QLabel(MainWindow)
         self.label.setGeometry(QtCore.QRect(325, 0, 200, 100))
         self.label.setStyleSheet(
             "color: white;\n"
             "font-family: bahnschrift;\n"
             "font-size: 60px;\n"
-            ""
-            )
+            "")
 
+
+#####################################################################
+#Buttons
 
         self.label.setObjectName("X")
         self.pushButtonE = QtWidgets.QPushButton(MainWindow)
@@ -52,10 +57,21 @@ class UIWindow(object):
             "color: white;\n"
             "font-size: 25px;\n"
             "\n"
-            ""
-            )
+            "")
+
+        self.label.setObjectName("Temperatur")
+        self.pushButton = QtWidgets.QPushButton(MainWindow)
+        self.pushButton.setGeometry(QtCore.QRect(335, 100, 140, 50))
+        self.pushButton.setStyleSheet(
+            "font-family: bahnschrift;\n"
+            "color: white;\n"
+            "font-size: 25px;\n"
+            "background-color: #580000;\n"
+            "\n"
+            "")
 
 
+    
 ######################################################################
 #Funktionen
 
@@ -64,9 +80,14 @@ class UIWindow(object):
 
 
 
+
         self.pushButtonE.clicked.connect(abbrechen)
 
+        #self.pushButton.clicked.connect()
+
         self.pushButtonE.setObjectName("pushButton")
+
+        self.pushButton.setObjectName("Temperatur")
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow) 
@@ -76,6 +97,7 @@ class UIWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "Yokai - Serverüberwachung"))
         self.label.setText(_translate("MainWindow", "YOKAI"))
         self.pushButtonE.setText(_translate("MainWindow", "X"))
+        self.pushButton.setText(_translate("MainWindow", "Temperatur"))
 
 
 if __name__ == "__main__":
