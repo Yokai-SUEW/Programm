749c7dd6b975d57153f35098b1d34ee0ac4a2323
celiltan7 <73793746+celiltan7@users.noreply.github.com>
2020-12-16 23:10:13 +0100

    BigUpdate-Table-StyleSheet

--- yokai.py
+++ yokai.py
@@ -21,72 +21,80 @@ myresult = mycursor.fetchall()
 for row in myresult:
     print(row)
 '''
+#############################################################
+#StyleSheet
+
+StyleSheet = '''
+QPushButton#X {
+    background-color: #201f1f;
+    border: 2px solid white;
+    border-radius: 25px;
+    font-family: Arial;
+    color: white;
+    font-size: 25px;
+}
+QPushButton#X:hover {
+    background-color: #201f1f;
+    border: 2px solid #201f1f;
+    border-radius: 25px;
+    font-family: Arial;
+    color: grey;
+    font-size: 30px;
+}
+QPushButton#X:pressed {
+    background-color: #201f1f;
+    border: 2px solid #201f1f;
+    color: #201f1f;
+}
+'''
 
 ##############################################################
 #Bildschirm
-
 
 class UIWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setGeometry(600, 350, 800, 480)
         MainWindow.setFixedSize(800, 480)
         MainWindow.setStyleSheet("background-color: #201f1f;")
+        MainWindow.setWindowTitle("Yokai-Serverüberwachung")
         MainWindow.setWindowIcon(QtGui.QIcon('../favicon.png'))
         self.tableWidget = QTableWidget(MainWindow)
-        self.tableWidget.setRowCount(6)
+        self.tableWidget.setRowCount(8)
         self.tableWidget.setColumnCount(4)
-        self.tableWidget.setGeometry(QtCore.QRect(20,80,400,400))
+        self.tableWidget.setGeometry(QtCore.QRect(30,90,500,300))
         self.tableWidget.setStyleSheet(
-            "border: 3px solid white;\n"
-            "border-radius: 30px;\n"
             "color: white;\n"
-            "background-color: grey;\n"
-            "width: 70px;\n"
-            "height: 50px;\n"
+            "background-color: black;\n"
+            "width: 70%;\n"
+            "height: 60%;\n"
+            "font-family: bahnschrift;\n"
+            "font-size: 20px;\n"
         )
 
         self.label = QtWidgets.QLabel(MainWindow)
-        self.label.setGeometry(QtCore.QRect(330, 0, 200, 100))
+        self.label.setGeometry(QtCore.QRect(330, 0, 200, 80))
         self.label.setStyleSheet(
-            "color: white;\n"
             "font-family: bahnschrift;\n"
             "font-size: 60px;\n"
-            ""
-            )
-
-        self.labelDB = QtWidgets.QLabel(MainWindow)
-        self.labelDB.setGeometry(QtCore.QRect(15, 200, 200, 100))
-        self.labelDB.setStyleSheet(
-            "color: lightgrey;\n"
-            "font-family: bahnschrift;\n"
-            "font-size: 30px;\n"
-            "")
+            "color: white;\n"
+        )
 
 
 #####################################################################
 #Buttons
 
+        layoutV = QtWidgets.QLabel(value)
         self.label.setObjectName("X")
         self.pushButtonE = QtWidgets.QPushButton(MainWindow)
+        self.pushButtonE.setSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.pushButtonE.setGeometry(QtCore.QRect(375, 410, 50, 50))
-        self.pushButtonE.setStyleSheet(
-            "border: 2px solid white;\n"
-            "border-radius: 25px;\n"
-            "font-family: Arial;\n"
-            "color: white;\n"
-            "font-size: 25px;\n"
-            "\n"
-            "")
-
+        self.pushButtonE.setStyleSheet(StyleSheet)
     
 ######################################################################
 #Funktionen
 
         def abbrechen():
             sys.exit()
-
-
-
 
         self.pushButtonE.clicked.connect(abbrechen)
 
@@ -97,12 +105,8 @@ class UIWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Yokai - Serverüberwachung"))
         self.label.setText(_translate("MainWindow", "YOKAI"))
         self.pushButtonE.setText(_translate("MainWindow", "X"))
-
-######################################################################
-#Tabelle
 
 
         
